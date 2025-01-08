38b892881c16b7392c9900755aadc22d978725a9
Andres Sepulveda <andressep.95@gmail.com>
2025-01-07 21:17:55 -0300

    initial commit

---
+++ src/test/java/cl/playground/SpringSecurityBackend/SpringSecurityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.playground.SpringSecurityBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
